1c6ff8d0bebb70a0371a8afad3399323116dd7fd
bferrandini <benferrandini@gmail.com>
2018-03-21 12:59:35 +0100

    Ajout de commentaires

--- srcs/main.cpp
+++ srcs/main.cpp
@@ -10,30 +10,30 @@ bool check_map(std::vector<std::string> const& carte)
 	{
 		for (int j = 0; j < carte[0].size(); ++j)
 		{
-			if(carte[i][j] == 'X')
-				lettre_x++;
-
-			if (carte[i][j] != 'X' && \
-				carte[i][j] != '1' && \
-				carte[i][j] != '0')
+			if(carte[i][j] == 'X')																// Si une des lettres de la map est un X
+				lettre_x++;																		// Alors le nombre de X augmente de 1.
+										
+			if (carte[i][j] != 'X' && \															// Si une des lettres de la map n'est ni un X
+				carte[i][j] != '1' && \															// Ni un 1
+				carte[i][j] != '0')																// Ni un 0
 			{
-				std::cout << "Il y à un '" << carte[i][j] << "' dans la map... " << std::endl;
-				std::cout << "ligne : " << i+1 << " !" << std::endl;
-				std::cout << "caractère : " << j+1 << " !" << std::endl;
-				return false;
+				std::cout << "Il y à un '" << carte[i][j] << "' dans la map... " << std::endl;	// Alors on précise quel lettre est étrange
+				std::cout << "ligne : " << i+1 << " !" << std::endl;							// Puis on indique quel ligne
+				std::cout << "caractère : " << j+1 << " !" << std::endl;						// Puis quel charactère de la ligne
+				return false;																	// Et puis il y a une erreur.
 			}
 		}
 	}
 
-	if(lettre_x == 0)
+	if(lettre_x == 0)																			// Si il n'y a aucun X dans la map
 	{
-		std::cout << "Il dois y avoir au minimum 1 'X' dans la map." << std::endl;
-		return false;
+		std::cout << "Il dois y avoir au minimum 1 'X' dans la map." << std::endl;				// Alors on indique le problème
+		return false;																			// Puis il y a une erreur.
 	}
 
-	if(lettre_x >= 2)
-	{
-		std::cout << "Il ne dois pas y avoir plus de 1 'X' dans la map." << std::endl;
+	if(lettre_x >= 2)																			// Si il y à plus de 1 X dans la map
+	{		
+		std::cout << "Il ne dois pas y avoir plus de 1 'X' dans la map." << std::endl;			// Alors
 		return false;
 	}
 
@@ -43,7 +43,8 @@ bool check_map(std::vector<std::string> const& carte)
 		{
 			if(carte[i].size() != carte[i-1].size())
 			{
-				std::cout << "La map n'est pas rectangulaire." << std::endl;					
+				std::cout << "La map n'est pas rectangulaire." << std::endl;
+				std::cout << "Ligne : " << i << " !" << std::endl;					
 				return false;
 			}
 		}
@@ -56,7 +57,7 @@ bool check_map(std::vector<std::string> const& carte)
 	{
 		if(carte.back()[i] != '1')
 		{
-			std::cout << "Il y a un trou au su de la map." << std::endl;
+			std::cout << "Il y a un trou au sud de la map." << std::endl;
 			return false;
 		}
 
