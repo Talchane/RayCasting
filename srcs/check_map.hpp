9c5dd84ab994e5fe846cdc8c9e63f43e0d6ecf1a
bferrandini <benferrandini@gmail.com>
2018-04-29 20:01:15 +0200

    Ajout de commentaires et optimisation du check de la map

--- srcs/check_map.hpp
+++ srcs/check_map.hpp
@@ -4,6 +4,8 @@
 
 bool check_map(std::vector<std::string> const& carte)
 {
+	bool erreur = false;
+
 	int lettre_x = 0;
 
 	for (unsigned int i = 0; i < carte.size(); ++i)												// Tant qu'on a pas parcouru toutes les lignes de la map
@@ -20,7 +22,7 @@ bool check_map(std::vector<std::string> const& carte)
 				std::cout << "Il y à un '" << carte[i][j] << "' dans la map... " << std::endl;	// Alors on précise quel lettre est étrange
 				std::cout << "ligne : " << i+1 << " !" << std::endl;							// Puis on indique quel ligne
 				std::cout << "caractère : " << j+1 << " !" << std::endl;						// Puis quel charactère de la ligne
-				return false;																	// Et puis il y a une erreur.
+				erreur = true;																	// Et puis il y a une erreur.
 			}
 		}
 	}
@@ -28,13 +30,13 @@ bool check_map(std::vector<std::string> const& carte)
 	if(lettre_x == 0)																			// Si il n'y a aucun 'X' dans la map
 	{
 		std::cout << "Il dois y avoir au minimum 1 'X' dans la map." << std::endl;				// Alors on indique le problème
-		return false;																			// Puis il y a une erreur.      
+		erreur = true;																			// Puis il y a une erreur.      
 	}
 
 	if(lettre_x >= 2)																			// Si il y à plus de 1 'X' dans la map
 	{		
 		std::cout << "Il ne dois pas y avoir plus de 1 'X' dans la map." << std::endl;			// Alors on indique le problème																										// Puis il y a une erreur.     
-		return false;																			// Puis il y a une erreur.
+		erreur = true;																			// Puis il y a une erreur.
 	}
 
 	for (unsigned int i = 0; i < carte.size(); ++i)												// Tant qu'on a pas parcouru toutes les lignes de la map
@@ -44,8 +46,9 @@ bool check_map(std::vector<std::string> const& carte)
 			if(carte[i].size() != carte[i-1].size())											// Si cette ligne n'est pas aussi grande que la ligne d'avant 
 			{
 				std::cout << "La map n'est pas rectangulaire." << std::endl;					// Alors on précise que la carte n'est pas rectangulaire
-				std::cout << "Ligne : " << i << " !" << std::endl;								// Puis on indique la ligne
-				return false;																	// Puis il y a une erreur
+				std::cout << "Ligne : " << i + 1 << " !" << std::endl;							// Puis on indique la ligne
+				erreur = true;																	// Puis il y a une erreur
+				break;
 			}
 		}
 	}
@@ -59,14 +62,14 @@ bool check_map(std::vector<std::string> const& carte)
 		{
 			std::cout << "Il y a un trou au sud de la map." << std::endl;						// Alors on indique qu'il y a un trou au sud de la map
 			std::cout << "charactère : " << i << " !" << std::endl;								// Puis on indique quel charactère de la ligne
-			return false;																		// Puis il y a une erreur.
+			erreur = true;																		// Puis il y a une erreur.
 		}
 
 		if(carte[0][i] != '1')																	// Si un des charactère de la première ligne de la map est différent du charactère '1'
 		{
 			std::cout << "Il y a un trou au nord de la map." << std::endl;						// Alors on indique qu'il y a un trou au nord de la map
 			std::cout << "charactère : " << i << " !" << std::endl;								// Puis on indique quel charactère de la ligne
-			return false;																		// Puis il y a une erreur.
+			erreur = true;																		// Puis il y a une erreur.
 		}
 	}
 
@@ -76,16 +79,19 @@ bool check_map(std::vector<std::string> const& carte)
 		{
 			std::cout << "Il manque un mur à l'ouest de la carte." << std::endl;				// Alors on indique qu'il y a un trou à l'ouest de la map
 			std::cout << "ligne numéro : " << i + 1 << "." << std::endl;						// Puis on indique quel ligne
-			return false;																		// Puis il y a une erreur.
+			erreur = true;																		// Puis il y a une erreur.
 		}
 
 		if(carte[i][carte[i].size()-1] != '1')													// Si Le dernier charactère de la i ème ligne est différent du charactère '1'
 		{
 			std::cout << "Il manque un mur à l'est de la carte." << std::endl;					// Alors on indique qu'il y a un trou à l'est de la map 
 			std::cout << "ligne numéro : "<< i + 1 << "." << std::endl;							// Puis on indique quel ligne
-			return false;																		// Puis il y a une erreur.
+			erreur = true;																		// Puis il y a une erreur.
 		} 
 	}
+
+	if(erreur == true)
+		return false;
 
 	std::cout << "Aucune erreur trouvée." << std::endl;											// Si aucune erreur n'a été trouvé, alors on le dit
 	return true;																				// Puis on sort de la fonction sans erreur.
--- srcs/main.cpp
+++ srcs/main.cpp
@@ -8,7 +8,7 @@
 
 #define WIDTH 1600
 #define HEIGHT 900
-#define VITESSE 0.2
+#define VITESSE 2
 #define ROTATION 1
 #define FOV M_PI/2
 
@@ -37,15 +37,24 @@ struct player
 	sf::Vector2f dir = sf::Vector2f(0, -1);
 	sf::Vector2f pos = get_x(lignes);
 
-	void move()
+	void move(bool sens, sf::Time temps)
 	{
-		pos.x = pos.x + dir.x * VITESSE;
-		pos.y = pos.y + dir.y * VITESSE;
+		if(sens == true)
+		{
+			pos.x = pos.x + dir.x * VITESSE * temps.asSeconds();
+			pos.y = pos.y + dir.y * VITESSE * temps.asSeconds();
+		}
+		else
+		{
+			pos.x = pos.x - dir.x * VITESSE * temps.asSeconds();
+			pos.y = pos.y - dir.y * VITESSE * temps.asSeconds();
+		}
+
 	}
-	void rot(bool sens)
+	void rot(bool sens, sf::Time temps)
 	{
 		float	tmp = dir.x;
-		int		degree = ROTATION * ((sens) ? -1 : 1);
+		float	degree = ROTATION * ((sens) ? -M_PI * temps.asSeconds() : M_PI * temps.asSeconds());
 
 		dir.x = dir.x * cos(degree) - dir.y * sin(degree);
 		dir.y = tmp * sin(degree) + dir.y * cos(degree);
@@ -59,68 +68,82 @@ struct player
 
 float dist_mur(sf::Vector2f const& dir, sf::Vector2f const& pos, std::vector<std::string> lignes)
 {
-	float dx;
-	float dy;
-	float x = pos.x;
-	float y = pos.y;
-
-	if (fabs(dir.x - pos.x) > fabs(dir.y - pos.y))
+	//which box of the map we're in
+	int mapX = (int)pos.x;
+	int mapY = (int)pos.y;
+	//length of ray from current position to next x or y-side
+	float sideDistX;
+	float sideDistY;
+	//length of ray from one x or y-side to next x or y-side
+	float deltaDistX = fabs(1 / dir.x);
+	float deltaDistY = fabs(1 / dir.y);
+	//what direction to step in x or y-direction (either +1 or -1)
+	int stepX;
+	int stepY;
+	int side; //was a NS or a EW wall hit?
+	(void)side;
+	//calculate step and initial sideDist
+	if (dir.x < 0)
 	{
-		dy = (dir.y - pos.y) / (dir.x - pos.x);
-		dx = ((dir.x - pos.x) < 0) ? -1 : 1;
+		stepX = -1;
+		sideDistX = (pos.x - mapX) * deltaDistX;
 	}
 	else
 	{
-		dx = (dir.x - pos.x) / (dir.y - pos.y);
-		dy = ((dir.y - pos.y) < 0) ? -1 : 1;
+		stepX = 1;
+		sideDistX = (mapX + 1.0 - pos.x) * deltaDistX;
 	}
-	while (lignes[(int)y][(int)x] != '1')
+	if (dir.y < 0)
 	{
-		x = x + dx;
-		y = y + dy;
+		stepY = -1;
+		sideDistY = (pos.y - mapY) * deltaDistY;
 	}
-	return (sqrt((x-pos.x)*(x-pos.x)+(y-pos.y)*(y-pos.y)));
-	/*
-	si |x2-x1| >= |y2-y1| alors
-		longueur := |x2-x1|
-	sinon
-		longueur := |y2-y1|
-	fin si
-	dx := (x2-x1) / longueur
-	dy := (y2-y1) / longueur
-	x := x1 + 0.5
-	y := y1 + 0.5
-	i := 1
-	tant que i ≤ longueur faire
-		setPixel ( E ( x ), E ( y ) )
-		x := x + dx
-		y := y + dy
-		i := i + 1
-	fin tant que
-	*/
+	else
+	{
+		stepY = 1;
+		sideDistY = (mapY + 1.0 - pos.y) * deltaDistY;
+	}
+	while (lignes[mapY][mapX] != '1')			// L'erreur est entre ces parenthèse.
+	{
+		if (sideDistX < sideDistY)
+		{
+			sideDistX = sideDistX + deltaDistX;
+			mapX = mapX + stepX;
+			side = 0;
+		}
+		else
+		{
+			sideDistY = sideDistY + deltaDistY;
+			mapY = mapY + stepY;
+			side = 1;
+		}
+	}
+	if (!side)
+		return ((mapX - pos.x + (1 - stepX) / 2) / dir.x);
+	else
+		return ((mapY - pos.y + (1 - stepY) / 2) / dir.y);
 }
 
-void afficher_mur(float const& dist_mur, sf::Image &image, unsigned int colonne, sf::Vector2f vec)
+void afficher_mur(float const& dist_mur, sf::Image &image, unsigned int colonne)
 {
 	unsigned short int ratio = HEIGHT * 0.5;
 	unsigned short int mur_h = (unsigned short int)(ratio / dist_mur);
 	unsigned short int rab_cl = (unsigned short int)((HEIGHT - mur_h) / 2);
 
-	std::cout <<"dist mur = "<< dist_mur << std::endl << "colonne = "<< colonne << std::endl << "vec = " << vec.x << " " << vec.y << std::endl << std::endl;
 
-	for (int i = 0; i < rab_cl; ++i)
+	for (int i = 0; i < rab_cl && i < HEIGHT; ++i)
 	{
-		image.setPixel(colonne, i, sf::Color::Red);
+		image.setPixel(colonne, i, sf::Color(80, 80, 80));  //80, 80, 80
 	}
 
-	for (int i = rab_cl; i < mur_h + rab_cl; ++i)
+	for (int i = rab_cl; i < mur_h + rab_cl && i < HEIGHT; ++i)
 	{
-		image.setPixel(colonne, i, sf::Color::Blue);
+		image.setPixel(colonne, i, sf::Color(20, 20, 20));  // 20 20 20
 	}
 
 	for (int i = mur_h + rab_cl; i < HEIGHT; ++i)
 	{
-		image.setPixel(colonne, i, sf::Color::Red);
+		image.setPixel(colonne, i, sf::Color(40, 40, 40));  // 40 40 40
 	}
 
 
@@ -143,7 +166,7 @@ void diviser_ray(const player &joueur, sf::Image &image, std::vector<std::string
 	test_x = vec.x;
 	test_y = vec.y;
 
-//	afficher_mur(dist_mur(vec, joueur.pos, lignes), image, w, vec);
+	//	afficher_mur(dist_mur(vec, joueur.pos, lignes), image, w, vec);
 
 	for (w = 0; w < WIDTH; ++w)
 	{
@@ -153,58 +176,69 @@ void diviser_ray(const player &joueur, sf::Image &image, std::vector<std::string
 
 		test_x = vec.x;
 		test_y = vec.y;
-		afficher_mur(dist_mur(vec, joueur.pos, lignes), image, w, vec);
+		afficher_mur(dist_mur(vec, joueur.pos, lignes), image, w);
 	}
 }
 
 
 int main()
 {
-
-	unsigned int taille_y = 0;
+	sf::Clock chrono;													// On crée un chrono.
+	sf::Time temps;														// On crée un temps.
 
 	while(getline(map, ligne))											// Tant qu'on a pas parcouru toutes les lignes de la map
 	{
-		lignes.push_back(ligne);										// Alors on ajoute dans notre tableau de chaîne de charactère la "ligne" ème ligne.
-		taille_y++;
+		lignes.push_back(ligne);										// Alors on ajoute dans notre tableau de chaîne de charactère la "ligne" ème ligne.													
 	}
 
 	if(!check_map(lignes))												// Puis on effectue une vérification de la map.
-		return EXIT_FAILURE;
+	return EXIT_FAILURE;												// Si une erreur est rencontrée, alors on quitte le programme.
 
-	player joueur;
-
-	//std::cout << joueur.pos.x << std::endl;
-	//std::cout << joueur.pos.y << std::endl;
+	player joueur;														// On crée un joueur.
 
 	sf::RenderWindow fenetre(sf::VideoMode(1600, 900), "RayCasting");	// Création d'une fenêtre de taille 1600 x 900 pixels nommée "RayCasting".
 	fenetre.setVerticalSyncEnabled(true);
 
 	sf::Event evenement;												// Création d'un évènement.
 
-	sf::Image image;
-	image.create(WIDTH, HEIGHT, sf::Color::Black);
+	sf::Image image;													// Déclaration d'une image.
+	image.create(WIDTH, HEIGHT, sf::Color::Black);						// Création de l'image.				
 
-	sf::Texture texture;
-	sf::Sprite spa;
+	sf::Texture texture;												// Création d'une texture.
+	sf::Sprite spa;														// Création d'un sprite.
 
 	while(fenetre.isOpen())												// Tant que la fenètre est ouverte
 	{
-		diviser_ray(joueur, image, lignes);
-		texture.loadFromImage(image);
-		spa.setTexture(texture);
-		
+		for (double long i = 0; i < 50000000; ++i)	{}					// (Simulation de pc pas puissant)
+
+		temps = chrono.restart();										// Réinisialisation le chrono et assignement de temps à sa valeur précédente.
+
+		diviser_ray(joueur, image, lignes);								// Euh...					
+
+		texture.loadFromImage(image);									// Chargement de l'image
+		spa.setTexture(texture);										// Changement de texture
+
 		while(fenetre.pollEvent(evenement))								// Tant qu'un évènement est détecté
 		{
 			if(evenement.type == sf::Event::Closed)						// Si l'évènement détecté est la fermeture de la fenêtre
-				fenetre.close();										// Alors on ferme la fenêtre.
-			if(sf::Keyboard::isKeyPressed(sf::Keyboard::Up))
-				joueur.move();
-			if(sf::Keyboard::isKeyPressed(sf::Keyboard::Left))
-				joueur.rot(false);
-			if(sf::Keyboard::isKeyPressed(sf::Keyboard::Right))
-				joueur.rot(true);
+				fenetre.close();										// Alors on ferme de la fenêtre.
+			
 		}
+
+		if(sf::Keyboard::isKeyPressed(sf::Keyboard::Escape))			// Si on appuie sur échape
+			fenetre.close();											// Alors on ferme la fenêtre
+
+		if(sf::Keyboard::isKeyPressed(sf::Keyboard::Up))				// Si on appuie sur la flèche du haut
+			joueur.move(true, temps);									// Alors on bouge le joueur
+
+		if(sf::Keyboard::isKeyPressed(sf::Keyboard::Down))
+			joueur.move(false, temps);
+
+		if(sf::Keyboard::isKeyPressed(sf::Keyboard::Left))
+			joueur.rot(false, temps);
+
+		if(sf::Keyboard::isKeyPressed(sf::Keyboard::Right))
+			joueur.rot(true, temps);
 
 		fenetre.clear(sf::Color::Black);								// Effacement de tout ce qui apparait sur la fenêtre avec la couleur noir.
 		fenetre.draw(spa);
